d9934459be19f1f4e7f260881011262aa202fd43
binbin_song <314144572@qq.com>
2019-04-12 15:58:35 +0800

    提交我的微信数据返回

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Log;
 use EasyWeChatComposer\EasyWeChat;
-use EasyWeChat\Factory;
 
 class WeChatController extends Controller
 {
@@ -18,23 +17,13 @@ class WeChatController extends Controller
     public function serve()
     {
       //  dd($user = session('wechat.oauth_user.default'));
-       /* Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
         $app->server->push(function($message){
-            return "欢迎关注 overtrue！";
+            return "欢迎关注 我的测试公众号！";
         });
-        return $app->server->serve();*/
 
-        $config = [
-            'app_id' => 'wxadc04d30b09de6a3',
-            'secret' => '80b598ac021684e520c52fc95a44bc28',
-            'token' => 'weixin',
-            'response_type' => 'array',
-            //...
-        ];
-        $app = Factory::officialAccount($config);
-        $response = $app->server->serve();
-        return $response;
+        return $app->server->serve();
     }
 }
